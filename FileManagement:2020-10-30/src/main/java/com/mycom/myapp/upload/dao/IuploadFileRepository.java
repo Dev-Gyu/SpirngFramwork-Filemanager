74374255514e6efdad116700597455b0ce895eb4
GyuHyeong <cjsworbehd13@gmail.com>
2020-10-30 18:36:43 +0900

    Add files via upload
    모든 페이지에 메인메뉴로 복귀하는 기능 추가함. 추후 CSS태그 이용하여 페이지 꾸며야될듯

---
+++ FileManagement:2020-10-30/src/main/java/com/mycom/myapp/upload/dao/IuploadFileRepository.java
@@ -0,0 +1,25 @@
+package com.mycom.myapp.upload.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.mycom.myapp.upload.model.UploadFileVO;
+
+public interface IuploadFileRepository {
+	int getMaxFileId();
+	void uploadFile(UploadFileVO file);
+	
+	List<UploadFileVO> getFileList(String directoryName);
+	List<UploadFileVO> getAllFileList();
+	List<UploadFileVO> getImageList(String directoryName);
+	
+	UploadFileVO getFile(int fileId);
+	
+	String getDirectoryName(int fileId);
+	void updateDirectory(HashMap<String, Object> map);
+	
+	void deleteFile(int fileId);
+	void updateFile(UploadFileVO file);
+	
+
+}
